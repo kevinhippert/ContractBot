bb9f2206a03356f655dfa4979a7620587eceb97d
Calico Seders <calico.seders@seiu.org>
2025-03-24 12:41:30 -0700

    react app up and running

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MainView from "./pages/mainView/MainView";
+
+function App() {
+  return (
+    <>
+      <MainView />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/mainView/MainView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainView() {
+  return <div>What's up what's up????</div>;
+}
+
+export default MainView;
