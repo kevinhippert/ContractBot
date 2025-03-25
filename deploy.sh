a113f544f37132b116ee88c9c6d613a7125e2c9b
David Mertz, Ph.D <mertz@gnosis.cx>
2025-03-25 16:30:44 -0400

    Re comment out log rotation

--- deploy.sh
+++ deploy.sh
@@ -28,8 +28,8 @@ echo "Rotating log files into $HOME/log/archive"
 mkdir -p $HOME/log/archive
 
 # Archive any old logs
-gzip $HOME/log/backend-*.log || true
-gzip $HOME/log/frontend-*.log || true
+# gzip $HOME/log/backend-*.log || true
+# gzip $HOME/log/frontend-*.log || true
 # mv $HOME/log/*.log.gz $HOME/log/archive || true
 
 # Shut down Node serve if running, or just anything on port 3000
