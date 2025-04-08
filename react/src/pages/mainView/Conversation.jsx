7158799fa1a8fc94c6b248e6698c6765ff5971b1
Calico Seders <calico.seders@seiu.org>
2025-04-08 15:07:24 -0700

    wip on formatting answers, CORS error

--- react/src/pages/mainView/Conversation.jsx
+++ react/src/pages/mainView/Conversation.jsx
@@ -1,45 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { Paper, Box, Alert, Typography } from "@mui/material";
 
-function formatAnswer(answer) {
-    for (let i = 0; i < answer.length; i++) {
-        let para = answer[i].toString().trim();
-        console.log(para, typeof(para));
-        // Change **bold** to <b>bold</b>
-        para = para.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
-        // Change *italic* to <i>italic</i>
-        para = para.replace(/\*(.*?)\*/g, "<i>$1</i>");
-        // Change _underline_ to <u>underline</u>
-        para = para.replace(/_(.*?)_/g, "<u>$1</u>");
-
-        // Various header levels
-        /*
-        if (para.match(/^####/)) {
-            para = `<Typography variant="h4">{para.slice(4)}</Typography>`;
-        }
-        else if (para.match(/^###/)) {
-            para = {variant: "h3", text=para.slice(3)}
-        }
-        else if (para.match(/^##/)) {
-            para = `<Typography variant="h2">{para.slice(2)}</Typography>`;
-        }
-        else if (para.match(/^#/)) {
-            para = `<Typography variant="h1">{para.slice(1)}</Typography>`;
-        }
-        else { */
-            para = {variant: "body1", text=para};
-        }
-        answer[i] = para;
-    }
-    console.log(answer);
-    return answer;
-}
-
-
 function Conversation({ messages, errorMessage, isQuerying }) {
+  const [answers, setAnswers] = useState([]);
   useEffect(() => {
     console.log(messages);
   }, [messages]);
+
+  useEffect(() => {
+    // format only messages with type answer
+    let answers = messages.filter((message) => message.type === "answer");
+    setAnswers(formatAnswer(answers));
+  }, [messages]);
+
+  function formatAnswer(answer) {
+    for (let i = 0; i < answer.length; i++) {
+      let para = answer[i].toString().trim();
+      console.log(para, typeof para);
+      // Change **bold** to <b>bold</b>
+      para = para.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
+      // Change *italic* to <i>italic</i>
+      para = para.replace(/\*(.*?)\*/g, "<i>$1</i>");
+      // Change _underline_ to <u>underline</u>
+      para = para.replace(/_(.*?)_/g, "<u>$1</u>");
+
+      // Various header levels
+      /*
+          if (para.match(/^####/)) {
+              para = `<Typography variant="h4">{para.slice(4)}</Typography>`;
+          }
+          else if (para.match(/^###/)) {
+              para = {variant: "h3", text=para.slice(3)}
+          }
+          else if (para.match(/^##/)) {
+              para = `<Typography variant="h2">{para.slice(2)}</Typography>`;
+          }
+          else if (para.match(/^#/)) {
+              para = `<Typography variant="h1">{para.slice(1)}</Typography>`;
+          }
+          else { */
+      para = { variant: "body1", text: para };
+      answer[i] = para;
+    }
+
+    console.log(answer);
+    return answer;
+  }
 
   return (
     <>
@@ -54,7 +60,11 @@ function Conversation({ messages, errorMessage, isQuerying }) {
                   marginTop: "5px",
                 }}
               >
-                { answer.map((para) => formatAnswer(m.text)) }
+                {answers.map((answer) => (
+                  <Typography variant={answer.variant}>
+                    {answer.text}
+                  </Typography>
+                ))}
               </Paper>
             ))}
           </Box>
