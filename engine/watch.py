b1e1f1685f1f195e5638d27d2180894d5f03eea4
David Mertz, Ph.D <mertz@gnosis.cx>
2025-04-09 13:40:08 -0400

    Tweak on what's echoed by engine

--- engine/watch.py
+++ engine/watch.py
@@ -55,8 +55,8 @@ def poll_queries(engine: str, token: str) -> None:
 
     elif response.status_code == 200:
         data = response.json()
-        print(f"{now} {data}", file=stderr, flush=True)
         if data["Topic"] is not None:
+            print(f"{now} {data}", file=stderr, flush=True)
             topic = data["Topic"]
             for Q in data["Queries"]:
                 seq, (query, model) = Q.popitem()
