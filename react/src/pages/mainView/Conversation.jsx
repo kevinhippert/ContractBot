676cce70d1656ca44d74f8c7069ece614bfd7c9c
David Mertz, Ph.D <mertz@gnosis.cx>
2025-04-09 10:39:53 -0400

    Try interspersing thread

--- react/src/pages/mainView/Conversation.jsx
+++ react/src/pages/mainView/Conversation.jsx
@@ -2,50 +2,50 @@ import React, { useEffect, useState } from "react";
 import { Paper, Box, Alert, Typography } from "@mui/material";
 
 function Conversation({ messages, errorMessage, isQuerying }) {
-  const [fullAnswer, setAnswer] = useState([]);
-  const [query, setQuery] = useState([]);
+  const [thread, setThread] = useState([]);
 
   useEffect(() => {
-    // format only messages with type answer
-    let fullAnswer = messages.filter((message) => message.type === "answer");
-    setAnswer(formatAnswer(fullAnswer));
-  }, [messages]);
+    setThread(formatThread(messages));
+  }, [messages])
 
-  useEffect(() => {
-    let queryText = messages.filter((message) => message.type === "question");
-    setQuery(queryText);
-  }, [messages]);
+  function formatThread(messages) {
+    for (let i = 0; i < message.length; i++) {
+        let message = messages[i];
+        if (message.type === "question") {
+              let para = message[i].text.toString().trim();
 
+              // Change **bold** to <b>bold</b>
+              para = para.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
+              // Change *italic* to <i>italic</i>
+              para = para.replace(/\*(.*?)\*/g, "<i>$1</i>");
+              // Change _underline_ to <u>underline</u>
+              para = para.replace(/_(.*?)_/g, "<u>$1</u>");
 
-  function formatAnswer(answer) {
-    for (let i = 0; i < answer.length; i++) {
-      let para = answer[i].text.toString().trim();
-
-      // Change **bold** to <b>bold</b>
-      para = para.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
-      // Change *italic* to <i>italic</i>
-      para = para.replace(/\*(.*?)\*/g, "<i>$1</i>");
-      // Change _underline_ to <u>underline</u>
-      para = para.replace(/_(.*?)_/g, "<u>$1</u>");
-
-      if (para.match(/^####/)) {
-          answer[i] = { variant: "h1", text: para };
-      }
-      else if (para.match(/^###/)) {
-          answer[i] = { variant: "h3", text: para };
-      }
-      else if (para.match(/^##/)) {
-          answer[i] = { variant: "h2", text: para };
-      }
-      else if (para.match(/^#/)) {
-          answer[i] = { variant: "h1", text: para };
-      }
-      else {
-          answer[i] = { variant: "body1", text: para };
-      }
+              if (para.match(/^####/)) {
+                  message[i] = { variant: "h1", text: para };
+              }
+              else if (para.match(/^###/)) {
+                  message[i] = { variant: "h3", text: para };
+              }
+              else if (para.match(/^##/)) {
+                  message[i] = { variant: "h2", text: para };
+              }
+              else if (para.match(/^#/)) {
+                  message[i] = { variant: "h1", text: para };
+              }
+              else {
+                  message[i] = { variant: "body1", text: para };
+              }
+            }
+            return message;
+          } else if (message.type === "message") {
+            for (let i = 0; i < message.length; i++) {
+              let para = message[i].text.toString().trim();
+              message[i] = { variant: "body2", text: para };
+            }
+          }
+      return messages;
     }
-    return answer;
-  }
 
   return (
     <>
@@ -60,14 +60,9 @@ function Conversation({ messages, errorMessage, isQuerying }) {
                   marginTop: "5px",
                 }}
               >
-                {query.map((q) => (
+                {thread.map((p) => (
                   <Typography variant="body2">
-                    {q.text}
-                  </Typography>
-                ))}
-                {fullAnswer.map((answer) => (
-                  <Typography variant={answer.variant}>
-                    {answer.text}
+                    {p.text}
                   </Typography>
                 ))}
               </Paper>
