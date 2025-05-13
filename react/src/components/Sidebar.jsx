8952070ac3437f3fb3f1a62d771cf3678d7455a8
Calico Seders <calico.seders@seiu.org>
2025-05-13 11:19:08 -0700

    wip on creating documents view with shared sidebar

--- react/src/components/Sidebar.jsx
+++ react/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useTopic, createTopic } from "../../contexts/TopicContext";
+import { useTopic, createTopic } from "../contexts/TopicContext";
 import {
   Box,
   List,
--- react/src/pages/documentsView/DocumentsView.jsx
+++ react/src/pages/documentsView/DocumentsView.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material/";
 
 function DocumentsView() {
-  return <div>DocumentsView</div>;
+  return (
+    <>
+      <Typography>Documents</Typography>
+    </>
+  );
 }
 
 export default DocumentsView;
